4ff3d1f4b5bba1980450b376a67d989d54683ccf
Giorgio <caparvigiorgio@yahoo.it>
2022-05-24 19:21:46 +0200

    First Configuration Eureka Server

--- covid-eureka/src/main/java/covidstatistics/covideureka/CovidEurekaApplication.java
+++ covid-eureka/src/main/java/covidstatistics/covideureka/CovidEurekaApplication.java
@@ -2,8 +2,11 @@ package covidstatistics.covideureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
+@EnableEurekaServer
 public class CovidEurekaApplication {
 
 	public static void main(String[] args) {
