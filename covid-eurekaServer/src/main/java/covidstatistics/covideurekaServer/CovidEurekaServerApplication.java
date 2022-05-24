583a33bd859f77544e4d48d995674f0249edb6c7
Giorgio <caparvigiorgio@yahoo.it>
2022-05-24 18:47:59 +0200

    Added Eureka Server

---
+++ covid-eurekaServer/src/main/java/covidstatistics/covideurekaServer/CovidEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package covidstatistics.covideurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidEurekaServerApplication.class, args);
+	}
+
+}
---
+++ covid-eurekaServer/src/test/java/covidstatistics/covideurekaServer/CovidEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package covidstatistics.covideurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
